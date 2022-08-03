49eebc80797ec33b426bd63f322f865ffe44c758
MarcSa <marc@marcvirtual.com>
2022-08-03 19:38:24 +0200

    numbers

---
+++ numbers.js
@@ -0,0 +1,10 @@
+let age             =   39
+let dogYears        =   (age + 1) / 7
+
+console.log(dogYears)
+
+let studentScore    =   15
+let maxScore        =   27
+let percent         =   studentScore / maxScore * 100
+
+console.log( percent + "%" )
