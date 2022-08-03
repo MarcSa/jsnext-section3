13e4c1820b0f2c97b944e5e3930663ffccc85916
MarcSa <marc@marcvirtual.com>
2022-08-03 18:48:55 +0200

    first commit

---
+++ stringsandvariables/strings.js
@@ -0,0 +1,13 @@
+let firstName   =   'Marc'
+let lastName    =   'Santamaria'
+let fullName    =   firstName + ' ' + lastName
+
+console.log(fullName)
+
+// Challenge
+
+let city        =   'Santpedor'
+let country     =   'Catalunya'
+let location    =   city + ', ' + country
+
+console.log(location)
