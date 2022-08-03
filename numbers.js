2bdd621d04ca34ba27bdaf3205652c019df43824
MarcSa <marc@marcvirtual.com>
2022-08-03 19:42:46 +0200

    numbers mod

--- numbers.js
+++ numbers.js
@@ -3,8 +3,8 @@ let dogYears        =   (age + 1) / 7
 
 console.log(dogYears)
 
-let studentScore    =   15
-let maxScore        =   27
+let studentScore    =   18
+let maxScore        =   20
 let percent         =   studentScore / maxScore * 100
 
-console.log( percent + "%" )
+console.log( `${percent}%` )
